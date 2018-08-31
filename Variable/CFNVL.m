4085fa2f8ac79cd145ce52d3112e0f489cd8307c
kouhei.takemoto <take64@citrus.tk>
2018-09-01 04:26:47 +0900

    NVL:実装

---
+++ Variable/CFNVL.m
@@ -0,0 +1,38 @@
+//
+//  CFNVL.m
+//  CitrusFerrum
+//
+//  Created by kouhei.takemoto on 2018/09/01.
+//  Copyright © 2018年 citrus.tk. All rights reserved.
+//
+
+#import "CFNVL.h"
+
+@implementation CFNVL
+
+#pragma mark - static method
+//
+// static method
+//
+
+// NVL関数
++ (id) compare:(id)compare replace:(id)replace
+{
+    if(compare == nil || [compare isEqual:[NSNull null]] == YES)
+    {
+        return replace;
+    }
+    return compare;
+}
+
+// NVL2関数
++ (id) compare:(id)compare value1:(id)value1 value2:(id)value2
+{
+    if(compare == nil || [compare isEqual:[NSNull null]] == YES)
+    {
+        return value2;
+    }
+    return value1;
+}
+
+@end
