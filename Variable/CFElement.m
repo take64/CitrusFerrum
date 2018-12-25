0c3371cd6b0b9d86d82f3e661d3005fa7a781b3e
kouhei.takemoto <take64@citrus.tk>
2018-12-26 02:10:18 +0900

    CFElementが追加されていなかった

---
+++ Variable/CFElement.m
@@ -0,0 +1,13 @@
+//
+//  CFElement.m
+//  CitrusFerrum
+//
+//  Created by kouhei.takemoto on 2018/12/21.
+//  Copyright © 2018 citrus.tk. All rights reserved.
+//
+
+#import "CFElement.h"
+
+@implementation CFElement
+
+@end
