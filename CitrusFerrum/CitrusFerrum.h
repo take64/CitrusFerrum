db64a9a9b6db81693cd88113a75e130d42ee3f99
kouhei.takemoto <take64@citrus.tk>
2019-10-06 21:09:51 +0900

    ライブラリ化

---
+++ CitrusFerrum/CitrusFerrum.h
@@ -0,0 +1,19 @@
+//
+//  CitrusFerrum.h
+//  CitrusFerrum
+//
+//  Created by kouhei.takemoto on 2019/10/06.
+//  Copyright © 2019 citrus.tk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CitrusFerrum.
+FOUNDATION_EXPORT double CitrusFerrumVersionNumber;
+
+//! Project version string for CitrusFerrum.
+FOUNDATION_EXPORT const unsigned char CitrusFerrumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CitrusFerrum/PublicHeader.h>
+
+
